288072f32192351c1c7c394998ea90c076ca6031
FedericoStroppiana <federico.stroppiana@edu.itspiemonte.it>
2022-11-25 00:54:28 +0100

    calculate and print result

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [numbers, setNumbers] = useState([
+    { value: 100, sign: "+", disabled: false },
+    { value: 30, sign: "+", disabled: false },
+    { value: 7, sign: "-", disabled: false },
+  ]);
+  const [result, setResult] = useState(123);
+
+  useEffect(() => {
+    const result = numbers.reduce((accumulator, currentValue) => {
+      if (!currentValue.disabled && currentValue.value) {
+        if (currentValue.sign === "+") {
+          return accumulator + currentValue.value;
+        } else {
+          return accumulator - currentValue.value;
+        }
+      } else return accumulator;
+    }, 0);
+    setResult(result);
+  }, [numbers]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setNumbers([...numbers, { value: 0, sign: "+", disabled: false }]);
+        }}
+      >
+        Add row
+      </button>
+      <ul>
+        {numbers.map((number, index) => (
+          <li key={index}>
+            <select
+              value={number.sign}
+              onChange={(e: any) => {
+                let tempNumbersCopy = [...numbers];
+                tempNumbersCopy[index].sign = e.target.value;
+                setNumbers(tempNumbersCopy);
+              }}
+            >
+              <option value="+">+</option>
+              <option value="-">-</option>
+            </select>
+            <input
+              type="number"
+              value={number.value}
+              onChange={(e: any) => {
+                let tempNumbersCopy = [...numbers];
+                if (e.target.value === "")
+                  tempNumbersCopy[index].value = e.target.value;
+                else tempNumbersCopy[index].value = parseInt(e.target.value);
+                setNumbers(tempNumbersCopy);
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+      <div>{result}</div>
     </div>
   );
 }
